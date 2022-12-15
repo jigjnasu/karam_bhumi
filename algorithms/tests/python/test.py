a27fafd4669ead1e4a64cca1d12368a1c36a548c
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-12-16 10:39:35 +1100

    test update

--- algorithms/tests/python/test.py
+++ algorithms/tests/python/test.py
@@ -1,21 +1,26 @@
 import sys
+import time
 import random
 
 def load_modules():
     sys.path.insert(0, '/home/shishya/git_hub/learning_arena/algorithms/code/python')
 
-def test_sort():
-    load_modules()
-
-    n = 10
+def get_random_list(n, r):
     v = []
     for i in range(n):
-        v.append(random.randint(0, 100))
-    print(v)
-    
+        v.append(random.randint(0, r))
+    return v
+
+def test_sort():
+    load_modules()
+    n = int(1e6)
+    r = int(1e9)
+
+    v = get_random_list(n, r)    
     import bubble
+    start_time = time.time()
     v = bubble.sort(v)
-    print(v)
+    print(f'bubble sort execution time == {time.time() - start} seconds')
     
 if __name__ == '__main__':
     test_sort()
