4b6c68b491bdf0d0496437071deb8bdab0fea465
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-12-16 16:16:53 +1100

    insertion sort

---
+++ algorithms/code/python/insertion.py
@@ -0,0 +1,7 @@
+def sort(v):
+    for i in range(1, len(v)):
+        j = i
+        while j > 0 and v[j - 1] > v[j]:
+            v[j - 1], v[j] = v[j], v[j - 1]
+            j = j -1
+    return v
--- algorithms/tests/python/test.py
+++ algorithms/tests/python/test.py
@@ -11,16 +11,26 @@ def get_random_list(n, r):
         v.append(random.randint(0, r))
     return v
 
+def test_bubble_sort(v):
+    import bubble
+    start_time = time.time()
+    v = bubble.sort(v)
+    print(f'bubble sort execution time    == {time.time() - start_time} seconds')
+
+def test_insertion_sort(v):
+    import insertion
+    start_time = time.time()
+    v = insertion.sort(v)
+    print(f'insertion sort execution time == {time.time() - start_time} seconds')
+
 def test_sort():
     load_modules()
     n = int(2e4)
     r = int(1e9)
 
     v = get_random_list(n, r)    
-    import bubble
-    start_time = time.time()
-    v = bubble.sort(v)
-    print(f'bubble sort execution time == {time.time() - start_time} seconds')
-    
+    test_bubble_sort(v)
+    test_insertion_sort(v)
+
 if __name__ == '__main__':
     test_sort()
