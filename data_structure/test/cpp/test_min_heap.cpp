23760aa00729d3ae344c012d8bd3a7024a05b480
Rakesh Kumar <cpp.rakesh@gmail.com>
2022-12-31 22:59:52 +1100

    min heap

---
+++ data_structure/test/cpp/test_min_heap.cpp
@@ -0,0 +1,50 @@
+#include "../../code/cpp/min_heap.h"
+
+#include <chrono>
+#include <random>
+#include <cassert>
+#include <algorithm>
+#include <iostream>
+
+int random(int s, int e) {
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(s, e);
+    return dist(mt);
+}
+
+std::vector<int> get_random_vector(int n) {
+    std::vector<int> v;
+    for (int i = 0; i < n; ++i)
+        v.emplace_back(random(-1e9, 1e9));
+    return v;
+}
+
+void print(const std::vector<int> &v) {
+    printf("---------------------------------------------------------------------------\n");
+    for (int e : v)
+        printf("%d ", e);
+    printf("\n---------------------------------------------------------------------------\n");
+}
+
+void test_1() {
+    std::vector<int> v = get_random_vector(1e6);
+    auto start = std::chrono::high_resolution_clock::now();
+    MinHeap mh;
+    for (int e : v)
+        mh.push(e);
+    std::vector<int> v_sorted = v;
+    std::sort(v_sorted.begin(), v_sorted.end());
+    for (std::size_t i = 0; i < v_sorted.size(); ++i) {
+        assert(mh.top() == v_sorted[i]);
+        mh.pop();
+    }
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start);
+    std::cout << "min heap test case 1 took " << duration.count() / 1e9 << " seconds" << std::endl;
+}
+
+int main() {
+    test_1();
+
+    return 0;
+}
